b1d31c12df302e419dabda001c000c4aefe80219
Dmitriy41 <dmitriy_koptev@inbox.ru>
2025-08-02 17:12:34 +0300

    HW1 is completed

--- hw01_hello_otus/main.go
+++ hw01_hello_otus/main.go
@@ -1,5 +1,15 @@
+// Package main переворачивает строку Hello, OTUS!
 package main
+
+//nolint:depguard
+import (
+	"fmt"
+
+	"golang.org/x/example/hello/reverse"
+)
 
 func main() {
 	// Place your code here.
+	myStr := "Hello, OTUS!"
+	fmt.Print(reverse.String(myStr))
 }
